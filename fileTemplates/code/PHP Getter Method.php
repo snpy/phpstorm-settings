ad243b9af51c2c74d68d40a7f4a6f66f8393a786
Marek Snopkowski <marek@kramsoft.pl>
2019-09-26 22:20:11 +0200

    PS-2019.2.2 <marek@snpy-30 Overwrite remote https://github.com/snpy/phpstorm-settings.git to local

--- fileTemplates/code/PHP Getter Method.php
+++
@@ -1,9 +0,0 @@
-/** @return ${TYPE_HINT} */
-${STATIC} public function get${NAME}()
-{
-#if (${STATIC} == "static")
-    return static::$${FIELD_NAME};
-#else
-    return $this->${FIELD_NAME};
-#end
-}
